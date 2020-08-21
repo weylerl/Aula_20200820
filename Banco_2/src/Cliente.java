9f6a7cf1863d1242ea6bbdd0c05f2b93bcdaaa07
Weyler N M Lopes <38050619+weylerl@users.noreply.github.com>
2020-08-20 21:44:08 -0400

    Atualizando classes.

--- Banco_2/src/Cliente.java
+++ Banco_2/src/Cliente.java
@@ -15,8 +15,19 @@ public class Cliente {
 		this.dataNasc = dataNasc;
 		this.rendaMensal = renda;
 	}
+	
+	public Cliente(String nome, String cPF, Date dataNasc) {
+		super();
+		this.nome = nome;
+		CPF = cPF;
+		this.dataNasc = dataNasc;
+		this.rendaMensal = 0.00;
+	}
+	
+	public void setCliente(Double renda) {
+		this.rendaMensal = renda;
+	}
 
-	@Override
 	public String toString() {
 		SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
 		return "Cliente [nome=" + nome + ", CPF=" + CPF + ", dataNasc=" + formatoData.format(dataNasc) + ", renda=" + rendaMensal + "]";
--- Banco_2/src/Principal.java
+++ Banco_2/src/Principal.java
@@ -7,7 +7,13 @@ public class Principal {
 		
 		Cliente c1 = new Cliente("Pedro Alves", "42145687691", new SimpleDateFormat("dd/MM/yyyy").parse("03/03/1978"), 12345.00);
 
+		Cliente c2 = new Cliente("Maria dos Santos", "12345678944", new SimpleDateFormat("dd/MM/yyyy").parse("13/08/1968"));
+		
 		System.out.println(c1.toString());
+		c1.setCliente(15000.00);
+		System.out.println(c1.toString());
+		
+		System.out.println(c2.toString());
 	}
 
 }
